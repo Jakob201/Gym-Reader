03048598ae844d49104320444d94fc0ccecd676b
Cie_ <89694839+Jakob201@users.noreply.github.com>
2025-09-04 16:26:00 +0200

    New Change from VS Code

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ function load(){
         document.getElementById("counter").innerHTML = localStorage.getItem("counter")
         
     }
+    if(localStorage.getItem("cost1") != null){
+        x = localStorage.getItem("cost1")
+        y = x - 2*x
+        document.getElementById("del-1").value = "1 Chapter ("+x+")"
+        document.getElementById("del-1").onclick = function(){
+            add(y)
+        }
+    }
+    
+}
+function settings(){
+    localStorage.setItem("cost1", prompt("How much for one chapter: "))
+    
+    location.reload()
 }
